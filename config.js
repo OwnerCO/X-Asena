f9d2da3d383f4d416eb7793f0348a284362ca1e8
OwnerCO <jazminqntro96@gmail.com>
2024-08-05 22:34:38 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const toBool = (x) => x === "true";
 const DATABASE_URL = process.env.DATABASE_URL || "./assets/database.db";
 module.exports = {
-  ANTILINK: toBool(process.env.ANTI_LINK) || false,
+  ANTILINK: toBool(process.env.ANTI_LINK) || true,
   LOGS: toBool(process.env.LOGS) || true,
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
   SESSION_ID: process.env.SESSION_ID ||null,
@@ -22,16 +22,16 @@ module.exports = {
   GOODBYE_MSG: process.env.GOODBYE_MSG || "Hi @user It was Nice Seeing you",
   AUTHOR: process.env.AUTHOR || "X-Electra",
   SUDO:
-    process.env.SUDO || "918113921898,919598157259,918590508376,919383400679",
+    process.env.SUDO || "51978194933",
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   OWNER_NAME: process.env.OWNER_NAME || "Neeraj-X0",
   HEROKU: toBool(process.env.HEROKU) || false,
   BOT_NAME: process.env.BOT_NAME || "X-Asena",
-  AUTO_READ: toBool(process.env.AUTO_READ) || false,
-  AUTO_STATUS_READ: toBool(process.env.AUTO_STATUS_READ) || false,
+  AUTO_READ: toBool(process.env.AUTO_READ) || true,
+  AUTO_STATUS_READ: toBool(process.env.AUTO_STATUS_READ) || true,
   PROCESSNAME: process.env.PROCESSNAME || "x-asena",
-  WORK_TYPE: process.env.WORK_TYPE || "private",
+  WORK_TYPE: process.env.WORK_TYPE || "public",
   SESSION_URL: process.env.SESSION_URL || "",
   DELETED_LOG: toBool(process.env.DELETED_LOG) || false,
   DELETED_LOG_CHAT: process.env.DELETED_LOG_CHAT || false,
